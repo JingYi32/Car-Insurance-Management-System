d4601b79dcc20c53322769aaf0454211873b4758
Heoi-CS <71484708+Heoi-CS@users.noreply.github.com>
2020-10-08 23:35:26 +0800

    hi

---
+++ IOOP Assignment - Car Insurance Management System/Main Page.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace IOOP_Assignment___Car_Insurance_Management_System
+{
+    partial class Main_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Main_Page";
+            this.Text = "Main_Page";
+            this.Load += new System.EventHandler(this.Main_Page_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
