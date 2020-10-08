b1ee139ca88750354798b82450ed87fe451097cb
sydneyng08 <sydneyng08@users.noreply.github.com>
2020-10-08 23:21:28 +0800

    ?

--- IOOP Assignment - Car Insurance Management System/05 Customer Profile.cs
+++ IOOP Assignment - Car Insurance Management System/05 Customer Profile.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
 
 namespace IOOP_Assignment___Car_Insurance_Management_System
 {
@@ -16,15 +15,6 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
         public Customer_Profile()
         {
             InitializeComponent();
-        }
-
-        private void tlpHistory_Paint(object sender, PaintEventArgs e)
-        {
-            lblBil.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlign.Center;
-            lblInsuranceID.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlign.Center;
-            lblStartdate.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlign.Center;
-            lblStatus.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlign.Center;
-            lblEnddate.TextAlign = (System.Drawing.ContentAlignment)HorizontalAlign.Center;
         }
     }
 }
