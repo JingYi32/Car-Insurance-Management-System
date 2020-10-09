1b627e25515f67d653654ba789b47941a77b7232
sydneyng08 <sydneyng08@users.noreply.github.com>
2020-10-09 18:26:50 +0800

    main page

--- IOOP Assignment - Car Insurance Management System/02 Main Page.Designer.cs
+++ IOOP Assignment - Car Insurance Management System/02 Main Page.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gabriola", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(457, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 92);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome !";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gabriola", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(387, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(338, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "What would you like to do today ?";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(198, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Registration of New Account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(610, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 61);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "My Profile";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(198, 293);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(279, 61);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Registration of a Policy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(610, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(279, 61);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Log Out";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(198, 421);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(279, 61);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Search using ID";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(610, 486);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(279, 61);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Main_Page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(1102, 757);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Main_Page";
             this.Text = "Main_Page";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- IOOP Assignment - Car Insurance Management System/02 Main Page.cs
+++ IOOP Assignment - Car Insurance Management System/02 Main Page.cs
@@ -16,5 +16,46 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
         {
             InitializeComponent();
         }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Exit exit = new Exit();
+            exit.comfirm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Registration_Form register = new Registration_Form();
+            this.Hide();
+            register.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Profile_of_Staff s_profile = new Profile_of_Staff();
+            this.Hide();
+            s_profile.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Insurance_Details ins_policy = new Insurance_Details();
+            this.Hide();
+            ins_policy.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Login_Page login = new Login_Page();
+            this.Hide();
+            login.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Searching_Page search = new Searching_Page();
+            this.Hide();
+            search.Show();
+        }
     }
 }
