c1031aef3939ec688b5d362fb0cd237189c068d3
Heoi-CS <71484708+Heoi-CS@users.noreply.github.com>
2020-10-23 09:54:21 +0800

    staff

--- IOOP Assignment - Car Insurance Management System/11 Profile of Staff.Designer.cs
+++ IOOP Assignment - Car Insurance Management System/11 Profile of Staff.Designer.cs
@@ -38,6 +38,14 @@
             this.picProfilePicture_MP = new System.Windows.Forms.PictureBox();
             this.btnBack_MP = new System.Windows.Forms.Button();
             this.dgvMP = new System.Windows.Forms.DataGridView();
+            this.showStaID = new System.Windows.Forms.Label();
+            this.showStaName = new System.Windows.Forms.Label();
+            this.showStaGender = new System.Windows.Forms.Label();
+            this.showStaPosition = new System.Windows.Forms.Label();
+            this.showStaContact = new System.Windows.Forms.Label();
+            this.showStaAddress = new System.Windows.Forms.Label();
+            this.lblLastName_MP = new System.Windows.Forms.Label();
+            this.showStaLastN = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picProfilePicture_MP)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMP)).BeginInit();
             this.SuspendLayout();
@@ -46,9 +54,9 @@
             // 
             this.lblMyProfile.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.lblMyProfile.Font = new System.Drawing.Font("Gabriola", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMyProfile.Location = new System.Drawing.Point(362, 28);
+            this.lblMyProfile.Location = new System.Drawing.Point(362, 20);
             this.lblMyProfile.Name = "lblMyProfile";
-            this.lblMyProfile.Size = new System.Drawing.Size(398, 76);
+            this.lblMyProfile.Size = new System.Drawing.Size(398, 70);
             this.lblMyProfile.TabIndex = 0;
             this.lblMyProfile.Text = "My Profile";
             this.lblMyProfile.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -58,7 +66,7 @@
             this.lblStaffID_MP.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
             this.lblStaffID_MP.AutoSize = true;
             this.lblStaffID_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStaffID_MP.Location = new System.Drawing.Point(98, 121);
+            this.lblStaffID_MP.Location = new System.Drawing.Point(56, 96);
             this.lblStaffID_MP.Name = "lblStaffID_MP";
             this.lblStaffID_MP.Size = new System.Drawing.Size(100, 51);
             this.lblStaffID_MP.TabIndex = 1;
@@ -68,17 +76,17 @@
             // 
             this.lblStaffName_MP.AutoSize = true;
             this.lblStaffName_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStaffName_MP.Location = new System.Drawing.Point(98, 172);
+            this.lblStaffName_MP.Location = new System.Drawing.Point(56, 135);
             this.lblStaffName_MP.Name = "lblStaffName_MP";
-            this.lblStaffName_MP.Size = new System.Drawing.Size(132, 51);
+            this.lblStaffName_MP.Size = new System.Drawing.Size(133, 51);
             this.lblStaffName_MP.TabIndex = 2;
-            this.lblStaffName_MP.Text = "Staff Name:";
+            this.lblStaffName_MP.Text = "First Name:";
             // 
             // lblGender_MP
             // 
             this.lblGender_MP.AutoSize = true;
             this.lblGender_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblGender_MP.Location = new System.Drawing.Point(98, 217);
+            this.lblGender_MP.Location = new System.Drawing.Point(56, 214);
             this.lblGender_MP.Name = "lblGender_MP";
             this.lblGender_MP.Size = new System.Drawing.Size(98, 51);
             this.lblGender_MP.TabIndex = 3;
@@ -88,7 +96,7 @@
             // 
             this.lblJobPosition_MP.AutoSize = true;
             this.lblJobPosition_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblJobPosition_MP.Location = new System.Drawing.Point(98, 262);
+            this.lblJobPosition_MP.Location = new System.Drawing.Point(56, 258);
             this.lblJobPosition_MP.Name = "lblJobPosition_MP";
             this.lblJobPosition_MP.Size = new System.Drawing.Size(139, 51);
             this.lblJobPosition_MP.TabIndex = 4;
@@ -99,7 +107,7 @@
             // 
             this.lblContact_MP.AutoSize = true;
             this.lblContact_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContact_MP.Location = new System.Drawing.Point(98, 307);
+            this.lblContact_MP.Location = new System.Drawing.Point(56, 298);
             this.lblContact_MP.Name = "lblContact_MP";
             this.lblContact_MP.Size = new System.Drawing.Size(188, 51);
             this.lblContact_MP.TabIndex = 5;
@@ -109,7 +117,7 @@
             // 
             this.lblAddress_MP.AutoSize = true;
             this.lblAddress_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAddress_MP.Location = new System.Drawing.Point(98, 352);
+            this.lblAddress_MP.Location = new System.Drawing.Point(56, 343);
             this.lblAddress_MP.Name = "lblAddress_MP";
             this.lblAddress_MP.Size = new System.Drawing.Size(106, 51);
             this.lblAddress_MP.TabIndex = 6;
@@ -140,12 +148,85 @@
             // dgvMP
             // 
             this.dgvMP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMP.Location = new System.Drawing.Point(98, 407);
+            this.dgvMP.Location = new System.Drawing.Point(26, 435);
             this.dgvMP.Name = "dgvMP";
             this.dgvMP.RowHeadersWidth = 51;
             this.dgvMP.RowTemplate.Height = 24;
-            this.dgvMP.Size = new System.Drawing.Size(694, 291);
+            this.dgvMP.Size = new System.Drawing.Size(762, 291);
             this.dgvMP.TabIndex = 10;
+            // 
+            // showStaID
+            // 
+            this.showStaID.AutoSize = true;
+            this.showStaID.Location = new System.Drawing.Point(162, 101);
+            this.showStaID.Name = "showStaID";
+            this.showStaID.Size = new System.Drawing.Size(118, 45);
+            this.showStaID.TabIndex = 11;
+            this.showStaID.Text = "Show Staff ID";
+            // 
+            // showStaName
+            // 
+            this.showStaName.AutoSize = true;
+            this.showStaName.Location = new System.Drawing.Point(195, 141);
+            this.showStaName.Name = "showStaName";
+            this.showStaName.Size = new System.Drawing.Size(145, 45);
+            this.showStaName.TabIndex = 12;
+            this.showStaName.Text = "Show First Name";
+            // 
+            // showStaGender
+            // 
+            this.showStaGender.AutoSize = true;
+            this.showStaGender.Location = new System.Drawing.Point(164, 219);
+            this.showStaGender.Name = "showStaGender";
+            this.showStaGender.Size = new System.Drawing.Size(116, 45);
+            this.showStaGender.TabIndex = 13;
+            this.showStaGender.Text = "Show Gender";
+            // 
+            // showStaPosition
+            // 
+            this.showStaPosition.AutoSize = true;
+            this.showStaPosition.Location = new System.Drawing.Point(201, 264);
+            this.showStaPosition.Name = "showStaPosition";
+            this.showStaPosition.Size = new System.Drawing.Size(151, 45);
+            this.showStaPosition.TabIndex = 14;
+            this.showStaPosition.Text = "Show Job Position";
+            // 
+            // showStaContact
+            // 
+            this.showStaContact.AutoSize = true;
+            this.showStaContact.Location = new System.Drawing.Point(250, 309);
+            this.showStaContact.Name = "showStaContact";
+            this.showStaContact.Size = new System.Drawing.Size(188, 45);
+            this.showStaContact.TabIndex = 15;
+            this.showStaContact.Text = "Show Contact Number";
+            // 
+            // showStaAddress
+            // 
+            this.showStaAddress.AutoSize = true;
+            this.showStaAddress.Location = new System.Drawing.Point(168, 348);
+            this.showStaAddress.Name = "showStaAddress";
+            this.showStaAddress.Size = new System.Drawing.Size(123, 45);
+            this.showStaAddress.TabIndex = 16;
+            this.showStaAddress.Text = "Show Address";
+            // 
+            // lblLastName_MP
+            // 
+            this.lblLastName_MP.AutoSize = true;
+            this.lblLastName_MP.Font = new System.Drawing.Font("Gabriola", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName_MP.Location = new System.Drawing.Point(56, 175);
+            this.lblLastName_MP.Name = "lblLastName_MP";
+            this.lblLastName_MP.Size = new System.Drawing.Size(129, 51);
+            this.lblLastName_MP.TabIndex = 17;
+            this.lblLastName_MP.Text = "Last Name:";
+            // 
+            // showStaLastN
+            // 
+            this.showStaLastN.AutoSize = true;
+            this.showStaLastN.Location = new System.Drawing.Point(191, 180);
+            this.showStaLastN.Name = "showStaLastN";
+            this.showStaLastN.Size = new System.Drawing.Size(143, 45);
+            this.showStaLastN.TabIndex = 18;
+            this.showStaLastN.Text = "Show Last Name";
             // 
             // Profile_of_Staff
             // 
@@ -153,6 +234,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 757);
             this.ControlBox = false;
+            this.Controls.Add(this.showStaLastN);
+            this.Controls.Add(this.lblLastName_MP);
+            this.Controls.Add(this.showStaAddress);
+            this.Controls.Add(this.showStaContact);
+            this.Controls.Add(this.showStaPosition);
+            this.Controls.Add(this.showStaGender);
+            this.Controls.Add(this.showStaName);
+            this.Controls.Add(this.showStaID);
             this.Controls.Add(this.dgvMP);
             this.Controls.Add(this.btnBack_MP);
             this.Controls.Add(this.picProfilePicture_MP);
@@ -188,5 +277,13 @@
         private System.Windows.Forms.PictureBox picProfilePicture_MP;
         private System.Windows.Forms.Button btnBack_MP;
         private System.Windows.Forms.DataGridView dgvMP;
+        private System.Windows.Forms.Label showStaID;
+        private System.Windows.Forms.Label showStaName;
+        private System.Windows.Forms.Label showStaGender;
+        private System.Windows.Forms.Label showStaPosition;
+        private System.Windows.Forms.Label showStaContact;
+        private System.Windows.Forms.Label showStaAddress;
+        private System.Windows.Forms.Label lblLastName_MP;
+        private System.Windows.Forms.Label showStaLastN;
     }
 }
--- IOOP Assignment - Car Insurance Management System/11 Profile of Staff.cs
+++ IOOP Assignment - Car Insurance Management System/11 Profile of Staff.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
 {
     public partial class Profile_of_Staff : Form
     {
+        OleDbConnection con = new OleDbConnection();
+        OleDbCommand cmd = new OleDbCommand();
+
         public Profile_of_Staff()
         {
             InitializeComponent();
@@ -31,6 +35,30 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
 
         private void Profile_of_Staff_Load(object sender, EventArgs e)
         {
+            con.ConnectionString = "Provider=Microsoft.JET.OLEDB.4.0;Data Source=IOOPAssignment.mdb;";
+            con.Open();
+
+            cmd.CommandText = "select * from staff where username = 'admin'";
+            cmd.Connection = con;
+            OleDbDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                showStaID.Text = dr[0].ToString();
+                showStaName.Text = dr[3].ToString();
+                showStaLastN.Text = dr[4].ToString();
+                showStaGender.Text = dr[5].ToString();
+                showStaPosition.Text = dr[6].ToString();
+                showStaGender.Text = dr[7].ToString();
+                showStaAddress.Text = dr[8].ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Record not found");
+            }
+            dr.Close();
+
             DataTable dt_MP = new DataTable();
 
             dt_MP.Columns.Add("Current Incharge Customer ID");
