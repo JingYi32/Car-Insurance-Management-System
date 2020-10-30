184e10846988dc898e6b11cd18613d7448734080
sydneyng08 <sydneyng08@users.noreply.github.com>
2020-10-31 01:39:49 +0800

    .

--- IOOP Assignment - Car Insurance Management System/03 Registration Form.cs
+++ IOOP Assignment - Car Insurance Management System/03 Registration Form.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Data.OleDb;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IOOP_Assignment___Car_Insurance_Management_System
@@ -21,6 +15,29 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
             InitializeComponent();
         }
 
+        private void Registration_Form_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Provider=Microsoft.JET.OLEDB.4.0;Data Source=IOOPAssignment.mdb;";
+            con.Open();
+
+            //Generate Customer ID
+            cmd.CommandText = "SELECT COUNT(*) AS [Customer_ID] FROM Customer";
+            cmd.Connection = con;
+            OleDbDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                int id_numbering = int.Parse(dr[0].ToString()) + 1;
+                string id_number = id_numbering.ToString("00000");
+                Save.insuranceid = "G" + id_number;
+                lblRE_CustomerID2.Text = Save.insuranceid;
+            }
+            dr.Close();
+
+            
+            cbbRE_Gender.Items.Add("Male");
+            cbbRE_Gender.Items.Add("Female");
+        }
+
         private void label2_Click(object sender, EventArgs e)
         {
 
@@ -33,15 +50,118 @@ namespace IOOP_Assignment___Car_Insurance_Management_System
             mainMenu.Show();
         }
 
+        private void txtRE_Name_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRE_Name.Text))
+            {
+                e.Cancel = true;
+                txtRE_Name.Focus();
+                errorProvider1.SetError(txtRE_Name, "Please Enter Name of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider1.SetError(txtRE_Name, "");
+                Save.CustName = txtRE_Name.Text;
+            }
+        }
+
+        private void txtRE_IDNo_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRE_IDNo.Text))
+            {
+                e.Cancel = true;
+                txtRE_IDNo.Focus();
+                errorProvider2.SetError(txtRE_IDNo, "Please Enter IC Number of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider2.SetError(txtRE_IDNo, "");
+                Save.CustIC = txtRE_IDNo.Text;
+            }
+        }
+
+        private void cbbRE_Gender_Validating(object sender, CancelEventArgs e)
+        {
+            if (cbbRE_Gender.SelectedIndex == -1)
+            {
+                e.Cancel = true;
+                cbbRE_Gender.Focus();
+                errorProvider3.SetError(cbbRE_Gender, "Please Select Gender of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider3.SetError(cbbRE_Gender, "");
+                Save.CustGender = cbbRE_Gender.SelectedItem.ToString();
+            }
+        }
+
+        private void txtRE_Address_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRE_Address.Text))
+            {
+                e.Cancel = true;
+                txtRE_Address.Focus();
+                errorProvider4.SetError(txtRE_Address, "Please Enter Address of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider4.SetError(txtRE_Address, "");
+                Save.CustAddress = txtRE_Address.Text;
+            }
+        }
+
+        private void txtRE_Contact_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRE_Contact.Text))
+            {
+                e.Cancel = true;
+                txtRE_Contact.Focus();
+                errorProvider5.SetError(txtRE_Contact, "Please Enter Contact Number of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider5.SetError(txtRE_Contact, "");
+                Save.CustPhone = txtRE_Contact.Text;
+            }
+
+        }
+
+        private void txtRE_Email_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRE_Email.Text))
+            {
+                e.Cancel = true;
+                txtRE_Email.Focus();
+                errorProvider6.SetError(txtRE_Email, "Please Enter Email of Customer.");
+            }
+            else
+            {
+                e.Cancel = false;
+                errorProvider6.SetError(txtRE_Email, "");
+                Save.CustEmail = txtRE_Email.Text;
+            }
+        }
+
         private void btnRE_Submit(object sender, EventArgs e)
         {
+            Save.customerid = lblRE_CustomerID2.Text;
+
+            cmd.CommandText = "INSERT into Customer values ('" + Save.customerid + "', '" + Save.CustName + "', '" + Save.CustIC + "', '" + Save.CustAddress + "', '" + Save.CustGender + "', '" + Save.CustPhone + "', '" + Save.CustEmail + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Customer Successfully Registered.");
+
+            Main_Page main_page = new Main_Page();
+            this.Hide();
+            main_page.Show();
 
         }
 
-        private void Registration_Form_Load(object sender, EventArgs e)
-        {
-            con.ConnectionString = "Provider=Microsoft.JET.OLEDB.4.0;Data Source=IOOPAssignment.mdb;";
-            con.Open();
-        }
     }
+            
 }
